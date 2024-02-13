54492b02fbb7fa23a1af7ad98a905f29e4b8afa0
john <dd@dd.com>
2024-02-13 22:45:06 +0800

    fix electrs build error

--- bitcoin-init.sh
+++ bitcoin-init.sh
@@ -3,4 +3,4 @@ apk add --no-cache jq
 sleep 10
 set -x
 while ! nc -z bitcoind 18443; do sleep 1; done
-curl --user mempool:mempool --data-binary '{"jsonrpc": "1.0", "id":"curltest", "method": "generatetoaddress", "params": [140, "bcrt1qurpvvrlwxagq3yrvztl4htrcwlrc86a3ajzkj3"] }' -H 'content-type: text/plain;' http://bitcoind:18443/
+curl --user mempool:mempool --data-binary '{"jsonrpc": "1.0", "id":"curltest", "method": "generatetoaddress", "params": [110, "bcrt1qsdf9c0kj6p29795r5lcj95pcx2ywcmpgm82r5j"] }' -H 'content-type: text/plain;' http://bitcoind:18443/
